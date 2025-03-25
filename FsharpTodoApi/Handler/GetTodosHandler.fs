924c0dcbd795781f0fef2dd9db689e70dbd718e3
maaaashi <doyaseizin.7@gmail.com>
2025-03-25 15:14:48 +0900

    GetTodosHandlerのレスポンスJSON構造を修正し、不要な型とハンドラーを削除

--- FsharpTodoApi/Handler/GetTodosHandler.fs
+++ FsharpTodoApi/Handler/GetTodosHandler.fs
@@ -8,8 +8,7 @@ type TodoJson =
       title: string
       check: bool }
 
-type TodosJson =
-    { todos: TodoJson list }
+type TodosJson = { todos: TodoJson list }
 
 
 type GetTodosHandlerResult =
@@ -18,18 +17,24 @@ type GetTodosHandlerResult =
 
 module GetTodosHandler =
     let private toResponseJson (todos: Todos) : TodosJson =
-        { todos = [] }
+        { todos =
+            todos
+            |> List.map (fun (Todo(todo, user)) ->
+                { id = todo.id |> TodoTaskId.value
+                  title = todo.title |> TodoTaskTitle.value
+                  check = todo.status |> TodoTaskStatus.value = "completed" }) }
 
     let handler: Task<GetTodosHandlerResult> =
         async {
             let todo: TodoTask =
-              { id = TodoTaskId "blog_1"
-                title = TodoTaskTitle "タイトル1"
-                status = TodoTaskStatus "本文1" }
+                { id = TodoTaskId "blog_1"
+                  title = TodoTaskTitle "タイトル1"
+                  status = TodoTaskStatus "本文1" }
+
             let user: User =
                 { id = UserId "user_1"
                   name = UserName "ユーザー1" }
 
-            return toResponseJson (Todos [ Todo (todo, user)]) |> Ok
+            return toResponseJson (Todos [ Todo(todo, user) ]) |> Ok
         }
         |> Async.StartAsTask
--- FsharpTodoApi/Program.fs
+++ FsharpTodoApi/Program.fs
@@ -1,7 +1,5 @@
 open System
 
-open System.Text.Json
-open System.Text.Json.Serialization
 open Microsoft.AspNetCore.Http
 open Microsoft.AspNetCore.Builder
 open Microsoft.AspNetCore.HttpLogging
@@ -11,30 +9,11 @@ open Microsoft.Extensions.DependencyInjection
 open System.Threading.Tasks
 open FsharpTodoApi.Handler
 
-type DiscriminatedUnionConverter<'T>() =
-    inherit JsonConverter<'T>()
-
-    override this.Read(reader: byref<Utf8JsonReader>, typeToConvert: Type, options: JsonSerializerOptions) =
-        // カスタム読み取りロジックを実装します
-        raise (NotImplementedException())
-
-    override this.Write(writer: Utf8JsonWriter, value: 'T, options: JsonSerializerOptions) =
-        // カスタム書き込みロジックを実装します
-        raise (NotImplementedException())
-
-type YourDiscriminatedUnionType =
-    | Case1 of string
-    | Case2 of int
-
 [<EntryPoint>]
 let main args =
     let builder = WebApplication.CreateBuilder(args)
 
     builder.Services.AddHttpLogging(fun logger -> logger.LoggingFields <- HttpLoggingFields.All)
-    |> ignore
-
-    builder.Services.Configure<JsonSerializerOptions>(fun (options: JsonSerializerOptions) ->
-        options.Converters.Add(DiscriminatedUnionConverter<YourDiscriminatedUnionType>()))
     |> ignore
 
     let app = builder.Build()
