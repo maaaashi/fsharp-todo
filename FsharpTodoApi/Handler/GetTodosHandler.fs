09210b289b19d1dfda50ebc62e269935d910e4bf
maaaashi <doyaseizin.7@gmail.com>
2025-04-03 23:30:34 +0900

    GetTodosHandlerにユーザー情報を追加し、レスポンスJSONの構造を更新

--- FsharpTodoApi/Handler/GetTodosHandler.fs
+++ FsharpTodoApi/Handler/GetTodosHandler.fs
@@ -4,28 +4,37 @@ open System.Threading.Tasks
 open Microsoft.AspNetCore.Http
 open FsharpTodoApi.Domain
 
+type UserJson =
+    { id: string
+      name: string }
+
 type TodoJson =
     { id: string
       title: string
-      check: bool }
+      check: bool
+      user: UserJson }
 
 type TodosJson = { todos: TodoJson list }
 
 module GetTodosHandler =
-    let private toResponseJson (Todos todos) : TodosJson =
+    let private toResponseJson (Todos (todos: Todo list)) : TodosJson =
         { todos =
             todos
             |> List.map (fun (todo, user) ->
-                { id = "1"
-                  title = "title"
-                  check = true }) }
+                { id = todo.id.ToString()
+                  title = todo.title.ToString()
+                  check = todo.status.Equals("done")
+                  user = {
+                    id = user.id.ToString()
+                    name = user.name.ToString()
+                  }}) }
 
     let handler: Task<IResult> =
         async {
             let todo: TodoTask =
                 { id = TodoTaskId "blog_1"
                   title = TodoTaskTitle "タイトル1"
-                  status = TodoTaskStatus "本文1" }
+                  status = TodoTaskStatus "done" }
 
             let user: User =
                 { id = UserId "user_1"
