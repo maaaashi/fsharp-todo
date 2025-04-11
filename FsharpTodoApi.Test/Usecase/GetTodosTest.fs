7f3369b4e6c94c0a14981890e4d0e9120210366f
maaaashi <doyaseizin.7@gmail.com>
2025-04-11 22:41:31 +0900

    テストプロジェクトの構成を更新し、GetTodosTestを追加。UnitTest1を削除し、paket.dependenciesおよびpaket.lockを新規作成。

--- FsharpTodoApi.Test/Usecase/GetTodosTest.fs
+++ FsharpTodoApi.Test/Usecase/GetTodosTest.fs
@@ -1,1 +1,16 @@
-module FsharpTodoApi.Usecase
+namespace FsharpTodoApi.Usecase
+
+open FsharpTodoApi.Usecase
+
+open System
+open FsUnit
+open NUnit.Framework
+
+
+[<TestFixture>]
+module GetTodosTest =
+  [<Test>]
+  let ``Todoを取得する``() =
+    async {
+
+    }
