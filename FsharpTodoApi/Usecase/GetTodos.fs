d23c0f5a2b93a296a771ea5ac8e124465bb85463
maaaashi <doyaseizin.7@gmail.com>
2025-04-11 22:23:37 +0900

    GetTodosモジュールのexecute関数を更新し、初期のTodoタスクとユーザー情報を返すように変更

--- FsharpTodoApi/Usecase/GetTodos.fs
+++ FsharpTodoApi/Usecase/GetTodos.fs
@@ -3,4 +3,14 @@ namespace FsharpTodoApi.Usecase
 open FsharpTodoApi.Domain
 
 module GetTodos =
-    let execute deps : Async<Todos> = async { return Todos [] }
+    let execute deps : Async<Todos> = async { return Todos [{
+        Task = {
+            Tid = TodoTaskId "1"
+            Title = TodoTaskTitle "Todo 1"
+            Status = TodoTaskStatus "Pending"
+        }
+        User = {
+            Uid = UserId "1"
+            Name = UserName "User 1"
+        }
+    }] }
